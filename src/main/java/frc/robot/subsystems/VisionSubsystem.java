87607b7f4fa93f4014ece377c42459d305a5c781
JavaCompiled <gvnostler@gmail.com>
2025-01-06 10:32:24 -0800

    Revert "no"
    This reverts commit 6424a4cc6fa4286e8c09e91fa58fa895fe16e319.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,31 +1,20 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
-
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.Drivetrain;
 import frc.robot.util.LimelightHelpers;
-import frc.robot.util.LimelightHelpers.RawFiducial;
 
 public class VisionSubsystem extends SubsystemBase {
     private final SwerveDriveSubsystem m_swerveDriveSubsystem;
-    private SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
-    private int up = 2;
 
     /**
      * Creates a new {@link VisionSubsystem}.
      */
     public VisionSubsystem(SwerveDriveSubsystem swerve) {
         this.m_swerveDriveSubsystem = swerve;
-        this.up = 2;
     }
 
     /**
@@ -34,31 +23,24 @@ public class VisionSubsystem extends SubsystemBase {
      * TODO: 1/4/2025
      */
     public void moveToAprilTagPeriodic() {
-        Pose3d killMe = LimelightHelpers.getTargetPose3d_CameraSpace("");
+        Pose3d aprilTagPosition = LimelightHelpers.getTargetPose3d_CameraSpace("");
 
-        double tagRotation = killMe.getRotation().getY();
+        double tagRotation = aprilTagPosition.getRotation().getY();
         double robotRotation = this.m_swerveDriveSubsystem.getPose().getRotation().getRadians();
         // Find the shortest path to said rotation.....
         double desiredRotation = Math.atan2(Math.sin(tagRotation - robotRotation), Math.cos(tagRotation - robotRotation));
         double angularSpeed = Math.max(-Constants.Vision.kMaxAngularSpeed, Math.min(Constants.Vision.kMaxAngularSpeed, Constants.Vision.kProportionalRotation * desiredRotation));
 
-        SmartDashboard.putNumber("robotRotation", robotRotation);
-        SmartDashboard.putNumber("cewdgyuewyuew", tagRotation);
-        SmartDashboard.putNumber("llehyugfu3yg", angularSpeed);
+        if (Math.abs(desiredRotation) < Constants.Vision.tolerance) {
+            return;
+        }
 
-        // if (Math.abs(robotRotation - desiredRotation) < Constants.Vision.tolerance) {
-        //     return;
-        // }
-        //this.m_swerveDriveSubsystem.driveRobotRelative(new ChassisSpeeds(0, 0, angularSpeed));
-        drive.withRotationalRate((angularSpeed * 0.8) * Drivetrain.MAX_TURN_VOLTAGE);
-        this.m_swerveDriveSubsystem.setControl(drive);
+        this.m_swerveDriveSubsystem.driveRobotRelative(new ChassisSpeeds(0, 0, angularSpeed));
     }
 
     @Override
     public void periodic() {
         // Temporary
-        up++;
-        if (up < 10) return;
         moveToAprilTagPeriodic();
     }
 
